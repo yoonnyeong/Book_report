9d47e3c9781cc2f7b437f0d7c031b1932fe0c984
yoonnyung <choiyn1994@naver.com>
2019-06-17 23:39:08 +0900

    editorpage complete

--- book_report/src/components/editor/EditorPane/EditorPane.js
+++ book_report/src/components/editor/EditorPane/EditorPane.js
@@ -6,75 +6,74 @@
 import React, { Component } from 'react';
 import styles from './EditorPane.scss';
 import classNames from 'classnames/bind';
+//Code mirror 코드에디터 라이브러리 -> 코드에 색상을 입혀주는 역할 
+import CodeMirror from 'codemirror';
 
-//import CodeMirror from 'codemirror';
+import 'codemirror/mode/markdown/markdown'; // 마크다운 문법 색상
+// 마크다운 내부에 들어가는 코드 색상
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/jsx/jsx';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/shell/shell';
 
-// import 'codemirror/mode/markdown/markdown'; // 마크다운 문법 색상
-// // 마크다운 내부에 들어가는 코드 색상
-// import 'codemirror/mode/javascript/javascript';
-// import 'codemirror/mode/jsx/jsx';
-// import 'codemirror/mode/css/css';
-// import 'codemirror/mode/shell/shell';
-
-// // CodeMirror를 위한 CSS 스타일
-// import 'codemirror/lib/codemirror.css';
-// import 'codemirror/theme/monokai.css';
+// CodeMirror를 위한 CSS 스타일
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/monokai.css';
 
 const cx = classNames.bind(styles);
 
 class EditorPane extends Component {
+  // 에디터 ref
+  editor= null
+  //CodeMirror 인스턴스
+  CodeMirror = null
 
-  // editor = null // 에디터 ref
-  // codeMirror = null // CodeMirror 인스턴스
-  // cursor = null // 에디터의 텍스트 cursor 위치
-  
-  // initializeEditor = () => {
-  //   this.codeMirror = CodeMirror(this.editor, {
-  //     mode: 'markdown',
-  //     theme: 'monokai',
-  //     lineNumbers: true, // 좌측에 라인 넘버 띄우기
-  //     lineWrapping: true // 내용이 너무 길면 다음 줄에 작성
-  //   });
-  //   this.codeMirror.on('change', this.handleChangeMarkdown);
-  // }
+  initializeEditor = () => {
+    this.CodeMirror = CodeMirror(this.editor, {
+      mode : 'markdown',
+      theme : 'monokai',
+      //왼쪽에 라인 넘버 띄우기
+      lineNumbers : true,
+      // 내용이 너무 길면 다음 줄에 작성
+      lineWrapping : true
+    });
+  }
+  //컴포넌트가 만들어지고 첫 렌더링을 다 마친후 실행되는 메소드 
+  componentDidMount(){
+    this.initializeEditor();
+  }
 
-  // componentDidMount() {
-  //   this.initializeEditor();
-  // }
+  handleChange = (e) => {
+    const { onChangeInput } = this.props;
+    const { value, name } = e.target;
+    onChangeInput({name, value});
+  }
 
-  // handleChange = (e) => {
-  //   const { onChangeInput } = this.props;
-  //   const { value, name } = e.target;
-  //   onChangeInput({name, value});
-  // }
+  handleChangeMarkdown = (doc) => {
+    const { onChangeInput } = this.props;
+    this.cursor = doc.getCursor(); // 텍스트 cursor의 위치를 저장
+    onChangeInput({
+      name: 'markdown',
+      value: doc.getValue()
+    });
+  }
 
-  // handleChangeMarkdown = (doc) => {
-  //   const { onChangeInput } = this.props;
-  //   this.cursor = doc.getCursor(); // 텍스트 cursor의 위치를 저장
-  //   onChangeInput({
-  //     name: 'markdown',
-  //     value: doc.getValue()
-  //   });
-  // }
-
-  // componentDidUpdate(prevProps, prevState) {
-  //   // markdown이 변경되면 에디터의 값도 변경해줌,
-  //   // 이 과정에서 텍스트 커서의 위치가 초기화되기 때문에, 
-  //   // 저장해둔 커서의 위치가 있으면 해당 위치로 설정!
-  //   if(prevProps.markdown !== this.props.markdown) {
-  //     const { codeMirror, cursor } = this;
-  //     if(!codeMirror) return; // 인스턴스가 아직 안 만들어진 경우
-  //     codeMirror.setValue(this.props.markdown);
-  //     if(!cursor) return; // 커서가 없는 경우
-  //     codeMirror.setCursor(cursor);
-  //   }
-  // }
-  
-  
+  componentDidUpdate(prevProps, prevState) {
+    // markdown이 변경되면 에디터의 값도 변경
+    // 이 과정에서 텍스트 커서의 위치가 초기화되기 때문에, 
+    // 저장해둔 커서의 위치가 있으면 해당 위치로 설정
+    if(prevProps.markdown !== this.props.markdown) {
+      const { codeMirror, cursor } = this;
+      if(!codeMirror) return; // 인스턴스가 아직 안 만들어진 경우
+      codeMirror.setValue(this.props.markdown);
+      if(!cursor) return; // 커서가 없는 경우
+      codeMirror.setCursor(cursor);
+    }
+  }
 
   render() {
-    // const { handleChange } = this;
-    // const { tags, title } = this.props;
+    const { handleChange } = this;
+    const { tags, title } = this.props;
 
     return (
       <div className={cx('editor-pane')}>
@@ -82,17 +81,17 @@ class EditorPane extends Component {
           className={cx('title')} 
           placeholder="제목을 입력하세요" 
           name="title"
-          // value={title}
-          // onChange={handleChange}
+          value={title}
+          onChange={handleChange}
         />
-        <div className={cx('code-editor')}></div>
+        <div className={cx('code-editor')} ref={ref=>this.editor=ref}></div>
         <div className={cx('tags')}>
           <div className={cx('description')}>태그</div>
           <input 
             name="tags"
             placeholder="태그를 입력하세요 (쉼표로 구분)"
-            // value={tags}
-            // onChange={handleChange}
+            value={tags}
+            onChange={handleChange}
           />
         </div>
       </div>
--- book_report/src/store/modules/editor.js
+++ book_report/src/store/modules/editor.js
@@ -1,11 +1,32 @@
-import {createAction , handleActions} from 'redux-actions' ; 
+/**
+ *  작성할 제목, 내용, 태그들의 상태를 리덕스에서 관리
+ * 
+ */
+import { createAction, handleActions } from 'redux-actions';
+
 import { Map } from 'immutable';
-import {pender} from 'redux-pender';
+import { pender } from 'redux-pender';
 
-//initial state
-const initialState = Map({});
+// action types
+const INITIALIZE = 'editor/INITIALIZE';
+const CHANGE_INPUT = 'editor/CHANGE_INPUT';
 
-//reducer
+// action creators
+export const initialize = createAction(INITIALIZE);
+export const changeInput = createAction(CHANGE_INPUT);
+
+// initial state
+const initialState = Map({
+  title: '',
+  markdown: '',
+  tags: ''
+});
+
+// reducer
 export default handleActions({
-
+  [INITIALIZE]: (state, action) => initialState,
+  [CHANGE_INPUT]: (state, action) => {
+    const { name, value } = action.payload;
+    return state.set(name, value);
+  }
 }, initialState)
